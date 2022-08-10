12ee227e816d93561bee72799f6ed53778bab879
J-724 <jjpernian@gmail.com>
2022-08-10 17:27:32 -0400

    Form component Transformed

--- dist/index.js
+++ dist/index.js
@@ -3594,93 +3594,52 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Experience_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Experience.js */ "./src/components/cvForm/Experience.js");
 /* harmony import */ var _PersonalInfo_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./PersonalInfo.js */ "./src/components/cvForm/PersonalInfo.js");
 /* harmony import */ var _misc_BtnsAddDel_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./misc/BtnsAddDel.js */ "./src/components/cvForm/misc/BtnsAddDel.js");
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 
 
 
 
 
-
-var Form = /*#__PURE__*/function (_Component) {
-  _inherits(Form, _Component);
-
-  var _super = _createSuper(Form);
-
-  function Form() {
-    _classCallCheck(this, Form);
-
-    return _super.apply(this, arguments);
-  }
-
-  _createClass(Form, [{
-    key: "render",
-    value: function render() {
-      var _this = this;
-
-      var educationElements = this.props.education.map(function (element) {
-        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_Education_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
-          key: element.id,
-          id: element.id,
-          degree: element.degree,
-          university: element.university,
-          start: element.start,
-          end: element.end,
-          about: element.about,
-          onChange: _this.props.ChangeEducation,
-          handleAdd: _this.props.AddEducation,
-          handleDelete: _this.props.DeleteEducation
-        });
-      });
-      var experienceElements = this.props.experience.map(function (element) {
-        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_Experience_js__WEBPACK_IMPORTED_MODULE_2__["default"], {
-          key: element.id,
-          id: element.id,
-          position: element.position,
-          company: element.company,
-          start: element.start,
-          end: element.end,
-          about: element.about,
-          onChange: _this.props.ChangeExperience,
-          handleAdd: _this.props.AddExperience,
-          handleDelete: _this.props.DeleteExperience
-        });
-      });
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
-        className: "container"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("fieldset", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("legend", null, "Personal Info"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_PersonalInfo_js__WEBPACK_IMPORTED_MODULE_3__["default"], {
-        personalInfo: this.props.personalInfo,
-        onChange: this.props.ChangePersonalInfo
-      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("fieldset", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("legend", null, "Education"), educationElements.length === 0 ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_BtnsAddDel_js__WEBPACK_IMPORTED_MODULE_4__.AddButton, {
-        handleAdd: this.props.AddEducation
-      }) : null, educationElements), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("fieldset", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("legend", null, "Experience"), experienceElements.length === 0 ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_BtnsAddDel_js__WEBPACK_IMPORTED_MODULE_4__.AddButton, {
-        handleAdd: this.props.AddExperience
-      }) : null, experienceElements));
-    }
-  }]);
-
-  return Form;
-}(react__WEBPACK_IMPORTED_MODULE_0__.Component);
+function Form(props) {
+  var educationElements = props.education.map(function (element) {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_Education_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
+      key: element.id,
+      id: element.id,
+      degree: element.degree,
+      university: element.university,
+      start: element.start,
+      end: element.end,
+      about: element.about,
+      onChange: props.ChangeEducation,
+      handleAdd: props.AddEducation,
+      handleDelete: props.DeleteEducation
+    });
+  });
+  var experienceElements = props.experience.map(function (element) {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_Experience_js__WEBPACK_IMPORTED_MODULE_2__["default"], {
+      key: element.id,
+      id: element.id,
+      position: element.position,
+      company: element.company,
+      start: element.start,
+      end: element.end,
+      about: element.about,
+      onChange: props.ChangeExperience,
+      handleAdd: props.AddExperience,
+      handleDelete: props.DeleteExperience
+    });
+  });
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "container"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("fieldset", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("legend", null, "Personal Info"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_PersonalInfo_js__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    personalInfo: props.personalInfo,
+    onChange: props.ChangePersonalInfo
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("fieldset", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("legend", null, "Education"), educationElements.length === 0 ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_BtnsAddDel_js__WEBPACK_IMPORTED_MODULE_4__.AddButton, {
+    handleAdd: props.AddEducation
+  }) : null, educationElements), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("fieldset", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("legend", null, "Experience"), experienceElements.length === 0 ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_BtnsAddDel_js__WEBPACK_IMPORTED_MODULE_4__.AddButton, {
+    handleAdd: props.AddExperience
+  }) : null, experienceElements));
+}
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Form);
 
--- src/components/cvForm/Form.js
+++ src/components/cvForm/Form.js
@@ -4,73 +4,70 @@ import Experience from "./Experience.js";
 import PersonalInfo from "./PersonalInfo.js";
 import {AddButton} from "./misc/BtnsAddDel.js";
 
-
-class Form extends Component {
-  render() {
-    const educationElements = this.props.education.map( element => {
-      return (
-        <Education 
-          key={element.id}
-          id={element.id}
-          degree={element.degree}
-          university={element.university}
-          start={element.start}
-          end={element.end}
-          about={element.about}
-          onChange={this.props.ChangeEducation}
-          handleAdd={this.props.AddEducation}
-          handleDelete={this.props.DeleteEducation}
-        />
-      )
-    });
-
-    const experienceElements = this.props.experience.map( element => {
-      return (
-        <Experience
-          key={element.id}
-          id={element.id}
-          position={element.position}
-          company={element.company}
-          start={element.start}
-          end={element.end}
-          about={element.about}
-          onChange={this.props.ChangeExperience}
-          handleAdd={this.props.AddExperience}
-          handleDelete={this.props.DeleteExperience}
-        />
-      )
-    })
-
+function Form (props) {
+  const educationElements = props.education.map( element => {
     return (
-      <div className="container">
-        <fieldset>
-          <legend>Personal Info</legend>
-          <PersonalInfo
-            personalInfo={this.props.personalInfo}
-            onChange={this.props.ChangePersonalInfo}
-          />
-        </fieldset >
-        <fieldset>
-          <legend>Education</legend>
-            {
-              (educationElements.length === 0) 
-              ? <AddButton handleAdd={this.props.AddEducation}/>
-              : null
-            }
-            {educationElements}
-        </fieldset>
-        <fieldset>  
-          <legend>Experience</legend>
-            {
-              (experienceElements.length === 0) 
-              ? <AddButton handleAdd={this.props.AddExperience}/>
-              : null
-            }
-            {experienceElements}
-        </fieldset>
-      </div>
-    );
-  }
+      <Education 
+        key={element.id}
+        id={element.id}
+        degree={element.degree}
+        university={element.university}
+        start={element.start}
+        end={element.end}
+        about={element.about}
+        onChange={props.ChangeEducation}
+        handleAdd={props.AddEducation}
+        handleDelete={props.DeleteEducation}
+      />
+    )
+  });
+
+  const experienceElements = props.experience.map( element => {
+    return (
+      <Experience
+        key={element.id}
+        id={element.id}
+        position={element.position}
+        company={element.company}
+        start={element.start}
+        end={element.end}
+        about={element.about}
+        onChange={props.ChangeExperience}
+        handleAdd={props.AddExperience}
+        handleDelete={props.DeleteExperience}
+      />
+    )
+  })
+
+  return (
+    <div className="container">
+      <fieldset>
+        <legend>Personal Info</legend>
+        <PersonalInfo
+          personalInfo={props.personalInfo}
+          onChange={props.ChangePersonalInfo}
+        />
+      </fieldset >
+      <fieldset>
+        <legend>Education</legend>
+          {
+            (educationElements.length === 0) 
+            ? <AddButton handleAdd={props.AddEducation}/>
+            : null
+          }
+          {educationElements}
+      </fieldset>
+      <fieldset>  
+        <legend>Experience</legend>
+          {
+            (experienceElements.length === 0) 
+            ? <AddButton handleAdd={props.AddExperience}/>
+            : null
+          }
+          {experienceElements}
+      </fieldset>
+    </div>
+  );
 }
 
 export default Form;  
