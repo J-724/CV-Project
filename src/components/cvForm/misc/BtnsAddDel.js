fed846f278d3cc7620852f4801ba6c163d0d1f49
J-724 <jjpernian@gmail.com>
2022-08-10 17:11:58 -0400

    Buttons  component Transformed

--- src/components/cvForm/misc/BtnsAddDel.js
+++ src/components/cvForm/misc/BtnsAddDel.js
@@ -1,31 +1,26 @@
 import React from "react";
 
-class AddButton extends React.Component {
-  render() {
-    return (
-      <button 
-      className='btn btn-secondary col-auto'
-      Style="margin-left: 5px" 
-      onClick={() => this.props.handleAdd()}
-      >
-          Add
-      </button>
-    )             
-  }
+function AddButton (props) {
+  return (
+    <button 
+    className='btn btn-secondary col-auto'
+    Style="margin-left: 5px" 
+    onClick={() => props.handleAdd()}
+    >
+        Add
+    </button>
+  )
 }
 
-class DelButton extends React.Component {
-  render() {
-    return (
-      <button 
-          className='btn btn-outline-secondary col-auto' 
-          onClick={() => this.props.handleDelete(this.props.id)}
-        >                
-            Delete
-        </button> 
-    )
-  }  
+function DelButton (props) {
+  return (
+    <button 
+        className='btn btn-outline-secondary col-auto' 
+        onClick={() => props.handleDelete(props.id)}
+      >                
+          Delete
+    </button> 
+  )
 }
-
 
 export { AddButton, DelButton };
