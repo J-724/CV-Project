622af3a732f56c6cb3df63be09ccea4810caeda1
J-724 <jjpernian@gmail.com>
2022-08-10 17:03:31 -0400

    TextArea component Transformed

--- src/components/cvForm/misc/TextArea.js
+++ src/components/cvForm/misc/TextArea.js
@@ -1,28 +1,23 @@
 import React from "react";
 
-
-
-class TextArea extends React.Component {
-  
-  render() {
-    return (
-      <div className="form-floating mb-3">
-        <textarea
-          className="form-control"
-          Style="height: 100px"
-          id={this.props.id}
-          value={this.props.value}
-          placeholder={this.props.placeholder}
-          name={this.props.name}
-          onChange={this.props.onChange()}
-        />
-        <label 
-          htmlFor={this.props.id}>
-            {this.props.name.charAt(0).toUpperCase() + this.props.name.slice(1)} 
-        </label>
-      </div>
-    );
-  }
-}
+function TextArea (props) {
+  return (
+    <div className="form-floating mb-3">
+      <textarea
+        className="form-control"
+        Style="height: 100px"
+        id={props.id}
+        value={props.value}
+        placeholder={props.placeholder}
+        name={props.name}
+        onChange={props.onChange()}
+      />
+      <label 
+        htmlFor={props.id}>
+          {props.name.charAt(0).toUpperCase() + props.name.slice(1)} 
+      </label>
+    </div>
+  );
+} 
 
 export default TextArea;
