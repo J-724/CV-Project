ab1526d9a0d34dbc8f0c937136d518c13d7f4e5b
J-724 <jjpernian@gmail.com>
2022-08-10 17:24:46 -0400

    Experience component Transformed

--- dist/index.js
+++ dist/index.js
@@ -3514,105 +3514,64 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./misc/TextInput.js */ "./src/components/cvForm/misc/TextInput.js");
 /* harmony import */ var _misc_TextArea_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./misc/TextArea.js */ "./src/components/cvForm/misc/TextArea.js");
 /* harmony import */ var _misc_BtnsAddDel_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./misc/BtnsAddDel.js */ "./src/components/cvForm/misc/BtnsAddDel.js");
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 
 
 
 
-
-var Experience = /*#__PURE__*/function (_Component) {
-  _inherits(Experience, _Component);
-
-  var _super = _createSuper(Experience);
-
-  function Experience() {
-    _classCallCheck(this, Experience);
-
-    return _super.apply(this, arguments);
-  }
-
-  _createClass(Experience, [{
-    key: "render",
-    value: function render() {
-      var _this = this;
-
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
-        className: "Experience",
-        Style: "margin-bottom: 9px"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
-        id: "".concat(this.props.id),
-        value: this.props.position,
-        placeholder: "Position",
-        name: "position",
-        onChange: function onChange(e) {
-          return _this.props.onChange(_this.props.id, e);
-        }
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
-        id: "".concat(this.props.id),
-        value: this.props.company,
-        placeholder: "Company",
-        name: "company",
-        onChange: function onChange(e) {
-          return _this.props.onChange(_this.props.id, e);
-        }
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
-        id: "".concat(this.props.id),
-        value: this.props.start,
-        placeholder: "Start",
-        name: "start",
-        onChange: function onChange(e) {
-          return _this.props.onChange(_this.props.id, e);
-        }
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
-        id: "".concat(this.props.id),
-        value: this.props.end,
-        placeholder: "End",
-        name: "end",
-        onChange: function onChange(e) {
-          return _this.props.onChange(_this.props.id, e);
-        }
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextArea_js__WEBPACK_IMPORTED_MODULE_2__["default"], {
-        id: "".concat(this.props.id),
-        value: this.props.about,
-        placeholder: "Write some details about",
-        name: "about",
-        onChange: function onChange(e) {
-          return _this.props.onChange(_this.props.id, e);
-        }
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
-        className: "btn-container d-grip d-flex justify-content-end"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_BtnsAddDel_js__WEBPACK_IMPORTED_MODULE_3__.DelButton, {
-        id: this.props.id,
-        handleDelete: this.props.handleDelete
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_BtnsAddDel_js__WEBPACK_IMPORTED_MODULE_3__.AddButton, {
-        handleAdd: this.props.handleAdd
-      })));
+function Experience(props) {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "Experience",
+    Style: "margin-bottom: 9px"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    id: "".concat(props.id),
+    value: props.position,
+    placeholder: "Position",
+    name: "position",
+    onChange: function onChange(e) {
+      return props.onChange(props.id, e);
     }
-  }]);
-
-  return Experience;
-}(react__WEBPACK_IMPORTED_MODULE_0__.Component);
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    id: "".concat(props.id),
+    value: props.company,
+    placeholder: "Company",
+    name: "company",
+    onChange: function onChange(e) {
+      return props.onChange(props.id, e);
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    id: "".concat(props.id),
+    value: props.start,
+    placeholder: "Start",
+    name: "start",
+    onChange: function onChange(e) {
+      return props.onChange(props.id, e);
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    id: "".concat(props.id),
+    value: props.end,
+    placeholder: "End",
+    name: "end",
+    onChange: function onChange(e) {
+      return props.onChange(props.id, e);
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextArea_js__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    id: "".concat(props.id),
+    value: props.about,
+    placeholder: "Write some details about",
+    name: "about",
+    onChange: function onChange(e) {
+      return props.onChange(props.id, e);
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "btn-container d-grip d-flex justify-content-end"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_BtnsAddDel_js__WEBPACK_IMPORTED_MODULE_3__.DelButton, {
+    id: props.id,
+    handleDelete: props.handleDelete
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_BtnsAddDel_js__WEBPACK_IMPORTED_MODULE_3__.AddButton, {
+    handleAdd: props.handleAdd
+  })));
+}
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Experience);
 
--- src/components/cvForm/Experience.js
+++ src/components/cvForm/Experience.js
@@ -3,56 +3,53 @@ import TextInput from './misc/TextInput.js';
 import TextArea from './misc/TextArea.js';
 import {AddButton, DelButton} from './misc/BtnsAddDel.js';
 
-class Experience extends Component {
-
-    render() {
-        return (
-            <div className="Experience" Style="margin-bottom: 9px">
-                <TextInput
-                    id={`${this.props.id}`}
-                    value={this.props.position}
-                    placeholder='Position'
-                    name='position'
-                    onChange={(e) => this.props.onChange(this.props.id, e)}
-                />
-                <TextInput
-                    id={`${this.props.id}`}
-                    value={this.props.company}
-                    placeholder='Company'
-                    name='company'
-                    onChange={(e) => this.props.onChange(this.props.id, e)}
-                />
-                <TextInput
-                   id={`${this.props.id}`}
-                   value={this.props.start}
-                   placeholder='Start'
-                   name='start'
-                   onChange={(e) => this.props.onChange(this.props.id, e)}
-                />
-                <TextInput
-                    id={`${this.props.id}`}
-                    value={this.props.end}
-                    placeholder='End'
-                    name='end'
-                    onChange={(e) => this.props.onChange(this.props.id, e)}
-                />
-                <TextArea
-                    id={`${this.props.id}`}
-                    value={this.props.about}
-                    placeholder='Write some details about'
-                    name='about'
-                    onChange={(e) => this.props.onChange(this.props.id, e)}
-                />
-                <div className='btn-container d-grip d-flex justify-content-end'>
-                        <DelButton
-                            id ={this.props.id}
-                            handleDelete={this.props.handleDelete}
-                        />
-                        <AddButton handleAdd={this.props.handleAdd}/>
-                </div>
+function Experience (props) {
+    return (
+        <div className="Experience" Style="margin-bottom: 9px">
+            <TextInput
+                id={`${props.id}`}
+                value={props.position}
+                placeholder='Position'
+                name='position'
+                onChange={(e) => props.onChange(props.id, e)}
+            />
+            <TextInput
+                id={`${props.id}`}
+                value={props.company}
+                placeholder='Company'
+                name='company'
+                onChange={(e) => props.onChange(props.id, e)}
+            />
+            <TextInput
+               id={`${props.id}`}
+               value={props.start}
+               placeholder='Start'
+               name='start'
+               onChange={(e) => props.onChange(props.id, e)}
+            />
+            <TextInput
+                id={`${props.id}`}
+                value={props.end}
+                placeholder='End'
+                name='end'
+                onChange={(e) => props.onChange(props.id, e)}
+            />
+            <TextArea
+                id={`${props.id}`}
+                value={props.about}
+                placeholder='Write some details about'
+                name='about'
+                onChange={(e) => props.onChange(props.id, e)}
+            />
+            <div className='btn-container d-grip d-flex justify-content-end'>
+                    <DelButton
+                        id ={props.id}
+                        handleDelete={props.handleDelete}
+                    />
+                    <AddButton handleAdd={props.handleAdd}/>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Experience;
