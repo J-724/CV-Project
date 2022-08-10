aa5f3ec07062730c60f581ece98d41dddd371cd8
J-724 <jjpernian@gmail.com>
2022-08-10 17:19:47 -0400

    Personalinfo component Transformed

--- dist/index.js
+++ dist/index.js
@@ -3780,101 +3780,60 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./misc/TextInput.js */ "./src/components/cvForm/misc/TextInput.js");
 /* harmony import */ var _misc_TextArea_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./misc/TextArea.js */ "./src/components/cvForm/misc/TextArea.js");
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 
 
 
-
-var PersonalInfo = /*#__PURE__*/function (_Component) {
-  _inherits(PersonalInfo, _Component);
-
-  var _super = _createSuper(PersonalInfo);
-
-  function PersonalInfo() {
-    _classCallCheck(this, PersonalInfo);
-
-    return _super.apply(this, arguments);
-  }
-
-  _createClass(PersonalInfo, [{
-    key: "render",
-    value: function render() {
-      var _this = this;
-
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
-        className: "Personal-Info"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
-        divId: "divID_name",
-        id: "IDname",
-        value: this.props.personalInfo.name,
-        placeholder: "name",
-        name: "name",
-        onChange: function onChange(e) {
-          return _this.props.onChange;
-        }
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
-        divId: "divID_email",
-        id: "IDemail",
-        value: this.props.personalInfo.email,
-        placeholder: "email",
-        name: "email",
-        onChange: function onChange(e) {
-          return _this.props.onChange;
-        }
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
-        divId: "divID_address",
-        id: "IDaddress",
-        value: this.props.personalInfo.address,
-        placeholder: "State, Country",
-        name: "address",
-        onChange: function onChange(e) {
-          return _this.props.onChange;
-        }
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
-        divId: "divID_SMedia",
-        id: "IDSMedia",
-        value: this.props.personalInfo.socialMedia,
-        placeholder: "Most relevant social media link",
-        name: "socialMedia",
-        onChange: function onChange(e) {
-          return _this.props.onChange;
-        }
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextArea_js__WEBPACK_IMPORTED_MODULE_2__["default"], {
-        divId: "divID_PIAbout",
-        id: "IDPIAbout",
-        value: this.props.personalInfo.quickBio,
-        placeholder: "Write about yourself",
-        name: "quickBio",
-        onChange: function onChange(e) {
-          return _this.props.onChange;
-        }
-      }));
+function PersonalInfo(props) {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "Personal-Info"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    divId: "divID_name",
+    id: "IDname",
+    value: props.personalInfo.name,
+    placeholder: "name",
+    name: "name",
+    onChange: function onChange(e) {
+      return props.onChange;
     }
-  }]);
-
-  return PersonalInfo;
-}(react__WEBPACK_IMPORTED_MODULE_0__.Component);
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    divId: "divID_email",
+    id: "IDemail",
+    value: props.personalInfo.email,
+    placeholder: "email",
+    name: "email",
+    onChange: function onChange(e) {
+      return props.onChange;
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    divId: "divID_address",
+    id: "IDaddress",
+    value: props.personalInfo.address,
+    placeholder: "State, Country",
+    name: "address",
+    onChange: function onChange(e) {
+      return props.onChange;
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextInput_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    divId: "divID_SMedia",
+    id: "IDSMedia",
+    value: props.personalInfo.socialMedia,
+    placeholder: "Most relevant social media link",
+    name: "socialMedia",
+    onChange: function onChange(e) {
+      return props.onChange;
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_misc_TextArea_js__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    divId: "divID_PIAbout",
+    id: "IDPIAbout",
+    value: props.personalInfo.quickBio,
+    placeholder: "Write about yourself",
+    name: "quickBio",
+    onChange: function onChange(e) {
+      return props.onChange;
+    }
+  }));
+}
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (PersonalInfo);
 
--- src/components/cvForm/PersonalInfo.js
+++ src/components/cvForm/PersonalInfo.js
@@ -2,54 +2,51 @@ import React, {Component} from 'react';
 import TextInput from './misc/TextInput.js';
 import TextArea from './misc/TextArea.js';
 
-class PersonalInfo extends Component {
-
-    render() {
-        return (
-            <div className="Personal-Info">
-                <TextInput
-                    divId= 'divID_name'
-                    id= 'IDname'
-                    value={this.props.personalInfo.name}
-                    placeholder='name'
-                    name='name'
-                    onChange={(e) => this.props.onChange}
-                />
-                <TextInput
-                    divId= 'divID_email'
-                    id= 'IDemail'
-                    value={this.props.personalInfo.email}
-                    placeholder='email'
-                    name='email'
-                    onChange={(e) => this.props.onChange}
-                />
-                <TextInput
-                   divId= 'divID_address'
-                   id= 'IDaddress'
-                   value={this.props.personalInfo.address}
-                   placeholder='State, Country'
-                   name='address'
-                   onChange={(e) => this.props.onChange}
-                />
-                <TextInput
-                    divId= 'divID_SMedia'
-                    id= 'IDSMedia'
-                    value={this.props.personalInfo.socialMedia}
-                    placeholder='Most relevant social media link'
-                    name='socialMedia'
-                    onChange={(e) => this.props.onChange}
-                />
-                <TextArea
-                    divId= 'divID_PIAbout'
-                    id= 'IDPIAbout'
-                    value={this.props.personalInfo.quickBio}
-                    placeholder='Write about yourself'
-                    name='quickBio'
-                    onChange={(e) => this.props.onChange}
-                />
-            </div>
-        )
-    }   
+function PersonalInfo (props) {
+    return (
+        <div className="Personal-Info">
+            <TextInput
+                divId= 'divID_name'
+                id= 'IDname'
+                value={props.personalInfo.name}
+                placeholder='name'
+                name='name'
+                onChange={(e) => props.onChange}
+            />
+            <TextInput
+                divId= 'divID_email'
+                id= 'IDemail'
+                value={props.personalInfo.email}
+                placeholder='email'
+                name='email'
+                onChange={(e) => props.onChange}
+            />
+            <TextInput
+               divId= 'divID_address'
+               id= 'IDaddress'
+               value={props.personalInfo.address}
+               placeholder='State, Country'
+               name='address'
+               onChange={(e) => props.onChange}
+            />
+            <TextInput
+                divId= 'divID_SMedia'
+                id= 'IDSMedia'
+                value={props.personalInfo.socialMedia}
+                placeholder='Most relevant social media link'
+                name='socialMedia'
+                onChange={(e) => props.onChange}
+            />
+            <TextArea
+                divId= 'divID_PIAbout'
+                id= 'IDPIAbout'
+                value={props.personalInfo.quickBio}
+                placeholder='Write about yourself'
+                name='quickBio'
+                onChange={(e) => props.onChange}
+            />
+        </div>
+    )
 }
 
 export default PersonalInfo;
